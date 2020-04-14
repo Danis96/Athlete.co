615ddded2adb30223f745927fed855ccbac27d92
Danis96 <danis.preldzic@gmail.com>
2020-04-14 09:12:46 +0200

    theme,debug

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  Color white = Color.fromRGBO(255, 255, 255, 1);
+  Color black = Color.fromRGBO(0, 0, 0, 1);
+  Color error = Colors.red;
+}
