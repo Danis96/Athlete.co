261956e56c5cca1409d4f1fc12fe1c22be250598
jusuf-el <58688886+jusuf-el@users.noreply.github.com>
2020-04-23 13:43:59 +0200

    Default device orientation; Play/Pause on tap

--- lib/interface/chewieVideoInterface.dart
+++ lib/interface/chewieVideoInterface.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:video_player/video_player.dart';
 
 abstract class ChewieVideoInterface {
    goToNextVideo(int index, PageController pageController);
    playVideo(BuildContext context);
    showOverlay(BuildContext context);
+   pauseVideo(VideoPlayerController controller);
 }
--- lib/view/chewieVideo/widgets/indicatorsOnVideo.dart
+++ lib/view/chewieVideo/widgets/indicatorsOnVideo.dart
@@ -1,62 +1,74 @@
+import 'package:attt/view_model/chewieVideoViewModel.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class IndicatorsOnVideo extends StatelessWidget {
+  final VideoPlayerController controller;
+  IndicatorsOnVideo(this.controller);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
+    return InkWell(
+      onTap: () {
+        ChewieVideoViewModel().pauseVideo(controller);
+      },
+      child: Container(
+        child: Column(
           children: <Widget>[
-            Container(
-              child: Text('Gimnastic Push ups',
-                  style: TextStyle(color: Colors.white)),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 650.0),
-              child: IconButton(
-                icon: Icon(Icons.comment),
-                onPressed: () {},
-                color: Colors.white,
-                iconSize: 40.0,
-              ),
-            ),
-          ],
-        ),
-        Row(
-          children: <Widget>[
-            Column(
+            Row(
               children: <Widget>[
                 Container(
-                  margin: EdgeInsets.only(top: 250.0),
-                  child: Text('x10',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 32.0,
-                          fontWeight: FontWeight.bold,
-                          fontStyle: FontStyle.italic)),
+                  child: Text('Gimnastic Push ups',
+                      style: TextStyle(color: Colors.white)),
                 ),
                 Container(
-                  child: Text('1/5 Sets',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                          fontStyle: FontStyle.italic)),
+                  margin: EdgeInsets.only(left: 650.0),
+                  child: IconButton(
+                    icon: Icon(Icons.comment),
+                    onPressed: () {},
+                    color: Colors.white,
+                    iconSize: 40.0,
+                  ),
                 ),
               ],
             ),
-            Container(
-              margin: EdgeInsets.only(left: 700.0, top: 250.0),
-              child: IconButton(
-                icon: Icon(Icons.fiber_manual_record),
-                onPressed: () {},
-                color: Colors.white,
-                iconSize: 40.0,
-              ),
+            Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 250.0),
+                      child: Text('x10',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32.0,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic)),
+                    ),
+                    Container(
+                      child: Text('1/5 Sets',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic)),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 700.0, top: 250.0),
+                  child: IconButton(
+                    icon: Icon(Icons.fiber_manual_record),
+                    onPressed: () {},
+                    color: Colors.white,
+                    iconSize: 40.0,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
