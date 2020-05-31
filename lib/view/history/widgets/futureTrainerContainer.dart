fc1dde28706f204b0937fb24b0067b83ac6d9450
jusuf-el <58688886+jusuf-el@users.noreply.github.com>
2020-06-01 00:40:00 +0200

    Refactoring in progress

---
+++ lib/view/history/widgets/futureTrainerContainer.dart
@@ -0,0 +1,24 @@
+import 'package:attt/view_model/historyViewModel.dart';
+import 'package:flutter/material.dart';
+import 'package:attt/view/history/widgets/futureWeekContainer.dart';
+
+Widget futureTrainerContainer(List<dynamic> finishedWeeksWithAthlete, int index,
+    String trainerName, String weekName, List<dynamic> workoutsList) {
+  return FutureBuilder(
+    future: HistoryViewModel().getTainerName(
+        finishedWeeksWithAthlete[index].toString().split('_')[0]),
+    builder: (BuildContext context, AsyncSnapshot snapshot) {
+      if (snapshot.hasData) {
+        if (snapshot != null) {
+          trainerName = snapshot.data[0].data['trainer_name'];
+        }
+        return futureWeekContainer(finishedWeeksWithAthlete, index, weekName,
+            trainerName, workoutsList);
+      } else {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    },
+  );
+}
--- lib/view/history/widgets/historyList.dart
+++ lib/view/history/widgets/historyList.dart
@@ -1,6 +1,6 @@
-import 'package:attt/view/history/widgets/finishedWeekContainer.dart';
 import 'package:attt/view_model/historyViewModel.dart';
 import 'package:flutter/material.dart';
+import 'package:attt/view/history/widgets/futureTrainerContainer.dart';
 
 Widget historyList(
     List<dynamic> finishedWeeksWithAthlete, List<dynamic> finishedWorkouts) {
@@ -14,39 +14,8 @@ Widget historyList(
       List<dynamic> workoutsList = [];
       workoutsList = HistoryViewModel()
           .getWorkoutList(finishedWorkouts, finishedWeeksWithAthlete, index);
-      return FutureBuilder(
-        future: HistoryViewModel().getTainerName(
-            finishedWeeksWithAthlete[index].toString().split('_')[0]),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot != null) {
-              trainerName = snapshot.data[0].data['trainer_name'];
-            }
-            return FutureBuilder(
-              future: HistoryViewModel().getWeekName(
-                  finishedWeeksWithAthlete[index].toString().split('_')[0],
-                  finishedWeeksWithAthlete[index].toString().split('_')[1]),
-              builder: (BuildContext context, AsyncSnapshot snapshot2) {
-                if (snapshot2.hasData) {
-                  if (snapshot2 != null) {
-                    weekName = snapshot2.data[0].data['name'];
-                  }
-                  return finishedWeekContainer(weekName, trainerName,
-                      workoutsList, finishedWeeksWithAthlete, index);
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      );
+      return futureTrainerContainer(
+          finishedWeeksWithAthlete, index, trainerName, weekName, workoutsList);
     },
   );
 }
