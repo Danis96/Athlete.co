a85b9b55d98ec85abd1100ed7280e88ae969eb81
jusuf-el <58688886+jusuf-el@users.noreply.github.com>
2020-06-14 23:48:51 +0200

    Investor changes - NADAM SE POSLJEDNJE

--- lib/view/trainingPlan/widgets/workoutContainer.dart
+++ lib/view/trainingPlan/widgets/workoutContainer.dart
@@ -19,19 +19,19 @@ Widget workoutContainer(
   return GestureDetector(
     onTap: () {
       TrainingPlanViewModel().navigateToWorkout(
-          userDocument,
-          userTrainerDocument,
-          userTrainerDocument.data['trainerID'],
-          snapshot2.data[index2]['name'],
-          snapshot.data[index].data['weekID'],
-          snapshot2.data[index2].data['workoutID'],
-          snapshot2.data[index2].data['warmup'],
-          context,
-          weekName,
-          snapshot2.data[index2].data['notes'],
-          true,
-          workoutIDs.contains(workoutID),
-          snapshot2.data[index2].data['tag'],
+        userDocument,
+        userTrainerDocument,
+        userTrainerDocument.data['trainerID'],
+        snapshot2.data[index2]['name'],
+        snapshot.data[index].data['weekID'],
+        snapshot2.data[index2].data['workoutID'],
+        snapshot2.data[index2].data['warmup'],
+        context,
+        weekName,
+        snapshot2.data[index2].data['notes'],
+        true,
+        workoutIDs.contains(workoutID),
+        snapshot2.data[index2].data['tag'],
       );
     },
     child: Container(
@@ -42,31 +42,61 @@ Widget workoutContainer(
           ),
           color: Colors.black),
       child: ListTile(
-        title: Text(
-          snapshot.data[index].data['name']
-                  .toString()
-                  .substring(0, 1)
-                  .toUpperCase() +
-              snapshot.data[index].data['name'].toString().substring(1) +
-              ' ' +
-              snapshot2.data[index2].data['name']
-                  .toString()
-                  .substring(0, 1)
-                  .toUpperCase() +
-              snapshot2.data[index2].data['name'].toString().substring(1) +
-              ' ' +
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              snapshot.data[index].data['name']
+                      .toString()
+                      .substring(0, 1)
+                      .toUpperCase() +
+                  snapshot.data[index].data['name'].toString().substring(1) +
+                  ' ' +
+                  snapshot2.data[index2].data['name']
+                      .toString()
+                      .substring(0, 1)
+                      .toUpperCase() +
+                  snapshot2.data[index2].data['name'].toString().substring(1),
+              style: TextStyle(
+                  color: workoutIDs.contains(workoutID)
+                      ? Colors.white60
+                      : Colors.white,
+                  fontFamily: 'Roboto',
+                  fontSize: SizeConfig.blockSizeVertical * 2.5,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: SizeConfig.blockSizeVertical + 0.01,
+            ),
+            Text(
               snapshot2.data[index2]['tag']
-                  .toString()
-                  .substring(0, 1)
-                  .toUpperCase() +
-              snapshot2.data[index2]['tag'].toString().substring(1),
-          style: TextStyle(
-              color: workoutIDs.contains(workoutID)
-                  ? Colors.white60
-                  : Colors.white,
-              fontFamily: 'Roboto',
-              fontSize: SizeConfig.blockSizeVertical * 2.5,
-              fontWeight: FontWeight.w500),
+                      .toString()
+                      .substring(0, 1)
+                      .toUpperCase() +
+                  snapshot2.data[index2]['tag'].toString().substring(1),
+              style: TextStyle(
+                  color: workoutIDs.contains(workoutID)
+                      ? Colors.white60
+                      : Colors.white,
+                  fontFamily: 'Roboto',
+                  fontSize: SizeConfig.blockSizeVertical * 2.5,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: SizeConfig.blockSizeVertical + 0.01,
+            ),
+            Text(
+              snapshot2.data[index2].data['numberOfExercises'].toString() +
+                  ' Exercises',
+              style: TextStyle(
+                  color: workoutIDs.contains(workoutID)
+                      ? Colors.white70
+                      : Colors.white,
+                  fontFamily: 'Roboto',
+                  fontSize: SizeConfig.blockSizeVertical * 2,
+                  fontWeight: FontWeight.w400),
+            ),
+          ],
         ),
         contentPadding: EdgeInsets.symmetric(
           vertical: SizeConfig.blockSizeVertical * 1,
@@ -113,17 +143,6 @@ Widget workoutContainer(
                   ),
                 ],
               ),
-        subtitle: Text(
-          snapshot2.data[index2].data['numberOfExercises'].toString() +
-              ' Exercises',
-          style: TextStyle(
-              color: workoutIDs.contains(workoutID)
-                  ? Colors.white70
-                  : Colors.white,
-              fontFamily: 'Roboto',
-              fontSize: SizeConfig.blockSizeVertical * 2,
-              fontWeight: FontWeight.w400),
-        ),
       ),
     ),
   );
